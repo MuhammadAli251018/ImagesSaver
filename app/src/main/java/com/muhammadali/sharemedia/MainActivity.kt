da4f3f89324d4cb03d2307838baac678d015e71e
muhammad ali <muhammad251018@gmail.com>
2023-07-08 23:32:41 +0300

    Added the view image feature in MainActivity
    -Edited the intent filter of 'MainActivity' to receive 'share images' & changed the launch mode to 'SingleTop'
    -Added the dependency of COIL
    -Added the logic in 'MainActivity' to receive the image & view it from the intent

--- app/src/main/java/com/muhammadali/sharemedia/MainActivity.kt
+++ app/src/main/java/com/muhammadali/sharemedia/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.muhammadali.sharemedia
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.rememberAsyncImagePainter
 import com.muhammadali.sharemedia.ui.theme.ShareMediaTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val Tag = "MainActivityT"
+    private var uri: MutableState<Uri?> = mutableStateOf(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onIntentReceived(intent = intent)
+
         setContent {
             ShareMediaTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,18 +39,69 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        
+                        DrawImage(uri.value)
+                    }
                 }
             }
         }
     }
+
+    private fun Intent.isTheWaitedIntent(action: String, typePrefix: String): Boolean {
+        return if (this.type != null) {
+            this.action.equals(action) && type!!.startsWith(typePrefix)
+        } else
+            false
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null)
+            onIntentReceived(intent)
+    }
+
+    private fun onIntentReceived(intent: Intent) {
+        val action = Intent.ACTION_SEND
+
+        if (intent.isTheWaitedIntent(action, "image/"))
+        {
+            uri.value = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+                intent.getParcelableExtra(Intent.EXTRA_STREAM) as Uri?
+            else
+                intent.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+
+            Log.d(Tag, "image uri: $uri")
+        }
+    }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen(uri: Uri?, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        DrawImage(uri = uri)
+    }
+}
+
+@Composable
+fun DrawImage(uri: Uri?) {
+    
+    val painter = if (uri !=null)
+        rememberAsyncImagePainter(uri)
+    else
+        painterResource(id = R.drawable.ic_launcher_foreground)
+    
+    Image(
+        modifier= Modifier.fillMaxWidth(),
+        painter = painter,
+        contentDescription = "Image"
     )
 }
 
@@ -41,6 +109,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ShareMediaTheme {
-        Greeting("Android")
+       MainScreen(uri = null)
     }
 }
